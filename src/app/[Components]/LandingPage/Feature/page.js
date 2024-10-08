387f1cd8ee21a08faef80d592463d5d1efbce451
adeelsarwar1 <adeel2694551@gmail.com>
2024-10-08 07:33:52 +0500

    Landing page Changes

--- src/app/[Components]/LandingPage/Feature/page.js
+++ src/app/[Components]/LandingPage/Feature/page.js
@@ -8,19 +8,19 @@ const FeaturesPage = () => {
           <h2 className=" text-white text-4xl font-bold text-center mb-12">Our Features</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div className="bg-gray-100 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold mb-2">Car Detection</h3>
+              <h3 className="text-lg lg:text-2xl font-semibold mb-1 lg:mb-2">Car Detection</h3>
               <p>Our AI-driven technology allows you to upload an image and instantly detect the car model with high accuracy.</p>
             </div>
             <div className="bg-gray-100 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold mb-2">3D Modeling & Customization</h3>
+              <h3 className="text-lg lg:text-2xl font-semibold mb-1 lg:mb-2">3D Modeling & Customization</h3>
               <p>Experience the future of car customization with our advanced 3D tools, enabling you to visualize and modify car designs.</p>
             </div>
             <div className="bg-gray-100 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold mb-2">Real-time Analysis</h3>
+              <h3 className="text-lg lg:text-2xl font-semibold mb-1 lg:mb-2">Real-time Analysis</h3>
               <p>Get real-time insights and analysis on car models and their specifications, enhancing your decision-making process.</p>
             </div>
             <div className="bg-gray-100 p-6 rounded-lg shadow-lg">
-              <h3 className="text-2xl font-semibold mb-2">User-Friendly Interface</h3>
+              <h3 className="text-lg lg:text-2xl font-semibold mb-1 lg:mb-2">User-Friendly Interface</h3>
               <p>Navigate through our platform with ease, thanks to our intuitive and user-friendly interface designed for all users.</p>
             </div>
           </div>
--- src/app/page.js
+++ src/app/page.js
@@ -25,18 +25,18 @@ const LandingPage = () => {
       <div className="flex flex-col md:flex-row items-center justify-between py-24 px-8 md:px-16">
         {/* Left Side: Header and Section Content */}
         <div className="md:w-3/5 flex flex-col justify-center items-center text-center animate-fadeIn h-full space-y-8">
-          <h1 className="text-5xl font-bold">AutoModel Insight</h1>
-          <p className="text-xl">Revolutionizing Car Detection and 3D Modeling</p>
+          <h1 className=" text-2xl lg:text-5xl font-bold">AutoModel Insight</h1>
+          <p className="text-lg lg:text-xl">Revolutionizing Car Detection and 3D Modeling</p>
           <a href="/Components/LandingPage/Feature" alt="none" className="inline-block px-10 py-3 bg-white text-black rounded-lg shadow-lg  hover:px-12 transition duration-300">
             Learn More
           </a>
           <div className="flex justify-around py-10 space-x-4">
             <div className="w-1/2 bg-white bg-opacity-10 p-6 hover:p-4 rounded-lg shadow-lg animate-slideIn">
-              <h2 className="text-2xl font-semibold mb-2">Car Detection</h2>
+              <h2 className="text-lg lg:text-2xl font-semibold mb-2">Car Detection</h2>
               <p>Upload an image and let our AI detect the car model instantly.</p>
             </div>
             <div className="w-1/2 bg-white bg-opacity-10 p-6 hover:p-4 rounded-lg shadow-lg animate-slideIn">
-              <h2 className="text-2xl font-semibold mb-2">3D Modeling & Customization</h2>
+              <h2 className="text-lg lg:text-2xl font-semibold mb-2">3D Modeling & Customization</h2>
               <p>Experience the future of car customization with our 3D tools.</p>
             </div>
           </div>
